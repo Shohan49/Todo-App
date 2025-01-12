cc32f0d09594a68e8bd8a115c5699934afe683ae
Abdul Azim Shohan <abdulazimshohan03@gmail.com>
2025-01-13 02:01:21 +0600

    divided component

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,13 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { formType } from '../form-type';
+import { EmptyListComponent } from './empty-list/empty-list.component';
 
 @Component({
   selector: 'app-todo-list',
-  imports: [CommonModule],
+  imports: [CommonModule , EmptyListComponent],
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.css'
 })
 export class TodoListComponent {
   @Input() todosList: formType[] = [];
+
+  delete(indexNumber: number) {
+    console.log('Delete the card', indexNumber);
+    this.todosList.splice(indexNumber, 1)
+  }
+  
 }
